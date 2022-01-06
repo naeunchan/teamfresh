dabcd11a5a5aa0167d091ebcdf5ebb62e4bf2c70
naeunchan <eunchan0492@naver.com>
2022-01-06 23:28:47 +0900

    배포 수정

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import styles from "../../styles";
 import Dropdown from "../Dropdown";
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,4 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
     media: {
         sm: "(max-width: 992px)",
